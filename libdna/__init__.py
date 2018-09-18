f1662a3a68c37299e576e8d946b62881064117b6
Antony Holmes <antony.b.holmes@gmail.com>
2018-09-17 20:01:14 -0400

    Initial commit

---
+++ libdna/__init__.py
@@ -0,0 +1,5 @@
+name = 'libdna'
+from libdna.libdna import Loc
+from libdna.libdna import DNA2Bit
+
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='libdna',
+    version='0.0.1',
+    author='Antony B Holmes',
+    author_email='antony.b.holmes@gmail.com',
+    description='A library for working with DNA.',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
